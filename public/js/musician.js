1430644a8cbcfb0f9968f4c2869fd9e97008ecf7
samirrajpurkar <samir.rajpurkar@gmail.com>
2014-12-24 13:06:46 -0800

    adds tempo

--- public/js/musician.js
+++ public/js/musician.js
@@ -6,36 +6,56 @@ var Musician = function(synth,instrument, notes) {
  	this.arpeggio_notes = ["C","E","G","C","G","E","C"];
  	this.triad_notes = ["G", "E", "C"];
  	this.bass_note = "C";
+ 	this.arpeggio_tempo = 0;
+ 	this.triad_tempo = 0;
+ 	this.bass_tempo = 0;
+ 	TIME = 1000; // delay time in seconds
  };
 
 
 
 Musician.prototype.arpeggio = function( ) {
-	var results= [ ];
+	var results= [ ]; //redundant
 	var that = this;
 	for (var count = 0; count < this.arpeggio_notes.length; count++) {
 		var seconds = (count+1) * 1000;
 		var result = window.setTimeout(function(count){
-			console.log(count);
-			console.log(that.arpeggio_notes[count]);
+			//console.log(count);
+			//console.log(that.arpeggio_notes[count]);
+			$("#arpeggio").html(that.arpeggio_notes[count]);
 			that.instrument.play(that.arpeggio_notes[count],4,2);
 		}, seconds, count);
 		
 		results.push(result);	
 	};
 	
-	return seconds; 	
+	return seconds; 	//redundant return. 
 
  };
 
  Musician.prototype.triad = function( ) { 
  	for (var count=0; count < this.triad_notes.length; count++){
+ 		//console.log(this.triad_notes[count]);
+ 		$("#triad").html(this.triad_notes);
  		this.instrument.play(this.triad_notes[count],4,2);
  	};
  };
 
  Musician.prototype.bass = function( ){ 
+ 	$("#bass").html(this.bass_note);
  	this.instrument.play(this.bass_note, 4, 2);
+ };
+
+ Musician.prototype.setArpeggioTempo = function( ){
+ 	this.arpeggio_tempo = this.arpeggio_notes.length * TIME;
+ };
+
+ Musician.prototype.setTriadTempo = function(tempo){
+ 	this.triad_tempo = tempo * TIME;
+ };
+
+ Musician.prototype.setBassTempo = function(tempo){ 
+ 	this.bass_tempo = tempo * TIME;
  };
 
 
--- public/js/orchestra.js
+++ public/js/orchestra.js
@@ -1,31 +1,43 @@
 var Orchestra = function( ) { 
-
+	this.t1 = 0;
+	this.t2 = 0;
+	this.t3 = 0;
 };
 
 Orchestra.prototype.start = function( ) { 
 	
     var musician_piano = new Musician(Synth,'piano', []);
-    var cycle_time = musician_piano.arpeggio( );
-    // console.log(cycle_time);
-    var t1 = window.setInterval(function( ){
-      musician_piano.arpeggio( );
-    }, cycle_time);
-
+    musician_piano.setArpeggioTempo();   
+    this.t1 = window.setInterval(function( ){
+      musician_piano.arpeggio();
+    },musician_piano.arpeggio_tempo);
+	
 
     
     var musician_organ = new Musician(Synth, 'organ', [ ]);
-    var t2 = window.setInterval(function( ){
+    musician_organ.setTriadTempo(3);
+    //console.log(musician_organ.triad_tempo);
+    
+    this.t2 = window.setInterval(function( ){
     	musician_organ.triad( );
-    }, 3000);
-
-    
-    
+    }, musician_organ.triad_tempo);
+	
 
     
     var musician_acoustic_guitar = new Musician(Synth, 'acoustic', []);
-    var t3 = window.setInterval(function( ){
+    //console.log(musician_acoustic_guitar.bass_tempo);
+    musician_acoustic_guitar.setBassTempo(3);
+    this.t3 = window.setInterval(function( ){
     	musician_acoustic_guitar.bass( );	
-    }, 3000);
+    }, musician_acoustic_guitar.bass_tempo);
     
     
+};
+
+Orchestra.prototype.stop = function( ) {
+	var that = this;
+	window.clearInterval(that.t1);
+	window.clearInterval(that.t2);
+	window.clearInterval(that.t3);
+
 };
