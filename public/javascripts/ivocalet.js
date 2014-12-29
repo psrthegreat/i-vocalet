1668c7f0b1f92ac0c849b7d1085828eb71e5370d
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-12-29 13:44:13 +0400

    converts global vars to local

--- public/javascripts/ivocalet.js
+++ public/javascripts/ivocalet.js
@@ -1,17 +1,16 @@
 var NOTES = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
-var OCTAVES = [2,3,4,5,6];
-var DURATION = 2; // in seconds
-var WAVEFILES = [ ];
-var instruments = ['piano', 'organ', 'acoustic'];
 
 var Ivocalet = function(synth) {
 	this.synth = synth;
 	this.instruments = [];
+	this.octaves = [4,5];
+	this.wavs = [];
+	this.instrumentString = ['piano', 'organ', 'acoustic'];
 };
 
 Ivocalet.prototype.createInstruments = function( ) {
-	for(var i = 0; i < instruments.length; i++){
-	   this.instruments.push(this.synth.createInstrument(instruments[i]));
+	for(var i = 0; i < this.instrumentString.length; i++){
+	   this.instruments.push(this.synth.createInstrument(this.instrumentString[i]));
 	}
 };
 
@@ -26,17 +25,17 @@ Ivocalet.prototype.init = function( ) {
  * "blob:http%3A//localhost%3A3000/4f8c344a-5d36-4f1c-b953-7a113094425d"  
  */
 Ivocalet.prototype.generateWaveFiles = function( ){
-	for(var ins = 0; ins < instruments.length; ins++){
+	for(var ins = 0; ins < this.instrumentString.length; ins++){
 		for (var i = 0; i < NOTES.length; i++) {
-			for (var j = 0; j < OCTAVES.length; j++) {
-				WAVEFILES.push(this.instruments[ins].generate(NOTES[i], OCTAVES[j], DURATION));
+			for (var j = 0; j < this.octaves.length; j++) {
+				this.wavs.push(this.instruments[ins].generate(NOTES[i], this.octaves[j], 2));
 			}
 		}
 	}
  };
 
  Ivocalet.prototype.test = function( ) { 
-	for(var i = 0; i < instruments.length; i++){
+	for(var i = 0; i < this.instrumentString.length; i++){
 		this.instruments[i].play('D', 4, 4);
 		this.instruments[i].play('F', 4, 4);
 		this.instruments[i].play('A', 4, 4);	
