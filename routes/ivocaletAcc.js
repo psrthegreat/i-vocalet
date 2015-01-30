9ac520a08ddd026461e36805ba139d29e486a93f
samirrajpurkar <samir.rajpurkar@gmail.com>
2015-01-30 07:18:54 -0800

    KnockOut integration
    This would be deleted in the next version

---
+++ routes/ivocaletAcc.js
@@ -0,0 +1,23 @@
+var ko = require('knockout');
+
+
+
+
+var NOTES = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
+
+
+db = [{
+		query: "nothing else matters",
+		lyrics: "this is string....",
+		chords: ['Em','D','C','G','B7','A'], 
+		notes: [{"Em":[4,7,11]}, 
+				{"D":[2,6,9]},
+				{"C":[0,4,7]},
+				{"G":[7,11,14]},
+				{"B7":[11,15,18,21]},
+				{"A":[9,13,16]}
+				]
+	}];
+
+
+
