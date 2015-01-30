1c473a8e7e18edc2e3c00b3632dea219aa0a518d
samirrajpurkar <samir.rajpurkar@gmail.com>
2015-01-30 07:16:55 -0800

    Content Management
    Content Handler that handles new music and get music API

---
+++ routes/content.js
@@ -0,0 +1,53 @@
+var MusicsDAO = require('./music').MusicsDAO;
+
+/* The ContentHandler must be constructed with a connected db */
+
+function ContentHandler(db){
+	"use strict";
+
+	var musics = new MusicsDAO(db);
+
+	this.displayMainMusicPage = function(req, res, next){
+		"use strict";
+		var query = "nothing else matters";
+
+		musics.getMusicByQuery(query, function(err, music){
+			"use strict"
+			if (err) return next(err);
+
+			return res.render('angmusic', music);
+		});
+	}
+
+
+	this.handleNewMusic = function(req, res, next){
+		"use strict"
+
+		var query = req.query.q;
+
+		musics.insertEntry(query, function(err, music){
+			"use strict"
+
+			if(err) return next(err);
+			
+			return res.render('accompaniment', music);
+		});
+	}
+
+	this.handleMusicJSON = function(req, res, next){
+		"use strict";
+		
+		var query = "nothing else matters";
+
+		musics.getMusicByQuery(query, function(err, music){
+			"use strict"
+			if (err) return next(err);
+
+			return res.json(music);
+		});
+
+	}
+
+}
+
+module.exports = ContentHandler;
